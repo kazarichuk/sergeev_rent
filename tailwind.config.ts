ec67fc5ff15d3d961479f45fb9f9ba6a96056e40
Kos <kazarichuk@mcookie.io>
2025-03-25 20:28:26 +0800

    Fix Tailwind and PostCSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -73,6 +73,6 @@ const config: Config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+} satisfies Config
 
 export default config 
