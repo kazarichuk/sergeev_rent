b8be7673a78fce99166dd71f555d5335d47d3f29
Kos <kazarichuk@mcookie.io>
2025-03-25 20:45:53 +0800

    Fix image loading and simplify design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,39 +10,30 @@ export default function Home() {
   return (
     <div className="gallery">
       <div className="image-container">
-        <Image
-          src="photo_2025-03-25 7.36.48 PM.jpeg"
+        <img
+          src="/photo_2025-03-25 7.36.48 PM.jpeg"
           alt="Автомобиль 1"
-          width={1200}
-          height={800}
-          priority
         />
       </div>
 
       <div className="image-container">
-        <Image
-          src="photo_2025-03-25 7.36.52 PM.jpeg"
+        <img
+          src="/photo_2025-03-25 7.36.52 PM.jpeg"
           alt="Автомобиль 2"
-          width={1200}
-          height={800}
         />
       </div>
 
       <div className="image-container">
-        <Image
-          src="photo_2025-03-25 7.36.54 PM.jpeg"
+        <img
+          src="/photo_2025-03-25 7.36.54 PM.jpeg"
           alt="Автомобиль 3"
-          width={1200}
-          height={800}
         />
       </div>
 
       <div className="image-container">
-        <Image
-          src="photo_2025-03-25 7.36.56 PM.jpeg"
+        <img
+          src="/photo_2025-03-25 7.36.56 PM.jpeg"
           alt="Автомобиль 4"
-          width={1200}
-          height={800}
         />
       </div>
     </div>
